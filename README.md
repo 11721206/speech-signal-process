2e2cb4fe4f2dcbb8fe7a8d827ae3d9b52397aca3
yuqiuz <zhou.yuqiu@hotmail.com>
2022-06-04 21:18:47 +0800

    提交零点约束代码

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # speech-signal-process
+
+# 运行环境
+-    MatLab 2016b
+# 资源说明
+-    data: 代码运行所需的数据
+-    ns: noise suppression 降噪
+-    beamforming: 波束成形
--- beamforming/beampattern.m
+++ beamforming/beampattern.m
@@ -11,17 +11,16 @@
 %-------------------------------------------------------------------------
 
 %% 画出频率波束响应图; 主波束方向为 90
-close all; clc; clear all;
+close all; clear all;clc;
 N = 11;
 c = 340;
 f = 1700;
 lamda = c/f;  
-d = lamda / 2;
+d = 0.05;%lamda / 2;
 xPos = -0.25:0.05:25;
 yPos = zeros(1, numel(xPos));
 zPos = zeros(1, numel(xPos));
-elementWeights = ones(1, numel(xPos)) / N;
-
+elementWeights = ones(1, N) / N;
 % 线性扫描的角度范围
 thetascanAngle = 0:0.001:3.1415;  % 从 0 到 pi
 phiScanAngles = 0;
@@ -58,5 +57,31 @@ figure;plot(thetascanAngle, 10 * log10(abs(B1).^2));title('ULA rect beam pattern
 %-------------------------------------------------------------------------
 %                            第三部分
 %-------------------------------------------------------------------------
-%% 零点调向 在 135 度形成零点调向
-null_angle = 3*pi/4;
+%% 零点调向在 155 度形成零点调向
+null_angle = 155/ 180 * pi;%3*pi/4;
+% N = 21;
+% thetascanAngle = -1:0.001:1;
+nulls = cos(null_angle) * 2 * d / lamda; %0.22
+Bnull = zeros(1, numel(thetascanAngle));
+elementWeights = ones(1, N) / N;
+v = zeros(N, 1);
+
+for k = 1 : N
+%     v(k) = exp(1j * ((k - 1) - (N - 1) / 2) * pi * nulls);
+    v(k) = exp(-1j * ((k - 1) - (N - 1) / 2) * (-2 * pi) / lamda * cos(null_angle) * d);
+end
+C = v; % 只有一个零点约束
+
+% 计算权值向量 Wo = Wd * (I - C * (C' * C)^-1 * C')
+
+Wo = (elementWeights *(eye(N) - C * inv(C' * C) * C'))';
+for i = 1 : numel(thetascanAngle)
+    for k = 1 : N
+        Bnull(i) = Bnull(i) + Wo(k) * exp(-1j * ((k - 1) - (N - 1) / 2) * (2 * pi * d) / lamda * cos(thetascanAngle(i)));
+%         Bnull(i) = Bnull(i) + Wo(k) * exp(-1j * ((k - 1) - (N + 1) / 2) * pi * cos(thetascanAngle(i)));
+    end
+end
+
+figure;hpol = polardb(thetascanAngle, 10 * log10(abs(Bnull).^2), -50);title('ULA polar beam pattern-零点调向');
+figure;plot(thetascanAngle, 10 * log10(abs(Bnull).^2));title('ULA rect beam pattern-零点调向'); grid on;
+
